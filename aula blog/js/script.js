3fd6255f29ab1c2ff30c25f6dce9a0d3a007ea93
melyssanimia-code <melyssanimia@gmail.com>
2026-05-26 01:04:49 +0000

    versao 1

---
+++ aula blog/js/script.js
@@ -0,0 +1,33 @@
+//js/script.js
+
+async function carregarPagina(pagina){
+
+     try{
+       
+        const resposta = await fetch(pagina);
+
+        if(!resposta.ok){
+            throw new Error("Página não encontraa");
+        }
+        
+        const ntml = await resposta.text();
+
+        document.getElementById("conteudo").innerHTML = ´
+
+    }catch(erro){
+    
+        document.getElementById("conteudo").innerHTML = ´
+            <h2>Erro</h2>
+            <p>Não foi possível carregar a página.</p>
+        `;
+
+        console.error(erro);
+    }
+}
+
+/*Página inicial*/
+
+window.onload = () => {
+    carregarPagina('pages/home.html');
+
+        
